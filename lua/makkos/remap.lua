4b38a4c7f9d0b344916ee52fff03af71d9414335
Thebearhead <46438485+Thebearhead@users.noreply.github.com>
2024-02-08 14:13:36 +0100

    Feat: Move lines with C-j + C-k(works in n and v)

--- lua/makkos/remap.lua
+++ lua/makkos/remap.lua
@@ -1,2 +1,8 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("n", "<C-j>", ":m+1<CR>", { silent = true})
+vim.keymap.set("n", "<C-k>", ":m-2<CR>", { silent = true})
+
+vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv", { silent = true})
+vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv", { silent = true})
